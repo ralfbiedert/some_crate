87fd7b87f8b8733142648b14a99aa9d04633c722
Ralf Biedert <rb@xr.io>
2021-05-10 19:37:22 +0200

    Bump version.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 #![no_std]
+//! This crate promises (in a non-legal sense) to be as side-effect-free as crate-possible:
+//!
+//! - should compile with Rust 1.0.0,
+//! - no dependencies,
+//! - `#![no_std]`,
+//! - doesn't set any app-global state,
+//! - only a handful of functions exported.
+//!
+//! Some non-default feature flags might be added later to opt into additional behavior.
+//!
 
 /// Does absolutely nothing.
 pub fn f() {}
